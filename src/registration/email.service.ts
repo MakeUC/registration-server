ad06a4faca092d658e3a15bdc610ef5e0f2de89f
Dev Agrawal <devagrawal09@gmail.com>
2020-06-29 03:43:09 -0400

    confirmation email template

--- src/registration/email.service.ts
+++ src/registration/email.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { readFile } from 'fs';
+import { Injectable, Logger } from '@nestjs/common';
 import * as sgMail from '@sendgrid/mail';
+import Handlebars from 'handlebars';
 import { Registrant } from './registrant.entity';
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -8,25 +10,42 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 export class EmailService {
   fromAddress = `contact@makeuc.io`;
 
-  async sendTestEmail(to: string): Promise<void> {
-    const msg: sgMail.MailDataRequired = {
-      to,
-      from: this.fromAddress,
-      subject: 'Sending with Twilio SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    sgMail.send(msg);
+  template: HandlebarsTemplateDelegate<{ fullName: string, verificationUrl: string, coverImgUrl: string }>;
+
+  constructor() {
+    this.getEmailTemplate();
+  }
+
+  getEmailTemplate(): void {
+    readFile(`${__dirname}/../../templates/index.html`, (err, data) => {
+      if(err) Logger.error(err);
+      if(!data) Logger.error(new Error(`Verification email template not found`));
+
+      this.template = Handlebars.compile(data.toString())
+    });
   }
 
   async sendVerificationEmail(registrant: Registrant): Promise<void> {
+    const fullName = registrant.fullName;
+    const verificationUrl = `${process.env.HOST}/registrant/verify/${registrant.id}`;
+    const coverImgUrl = `${process.env.WEBSITE_URL}/email/cover.png`;
+
+    if(!this.template) {
+      return Logger.error(`Verification email template not found`);
+    }
+
     const msg: sgMail.MailDataRequired = {
       to: registrant.email,
       from: this.fromAddress,
       subject: 'MakeUC registration',
       text: 'Confirm your email with MakeUC',
-      html: `Click here to confirm your email with makeuc: <a href="${process.env.HOST}/registrant/verify/${registrant.id}" target="_blank">here</a>`
+      html: this.template({ fullName, verificationUrl, coverImgUrl })
     };
-    sgMail.send(msg);
+
+    sgMail.send(msg).then(() => {
+      Logger.log(`Verification email sent to ${registrant.email} successfully`);
+    }).catch(err => {
+      Logger.error(`Verification email could not be sent to ${registrant.email}: ${err.message}`);
+    });
   }
 }
