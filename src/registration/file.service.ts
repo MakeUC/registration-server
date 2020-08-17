de0334172941c5e5a6c9aabc2cc42e9003e2f537
Dev Agrawal <devagrawal09@gmail.com>
2020-08-17 08:08:04 +0530

    bucket name provided by environment

--- src/registration/file.service.ts
+++ src/registration/file.service.ts
@@ -2,22 +2,27 @@ import { Injectable, Logger, HttpException, HttpStatus } from "@nestjs/common";
 import { S3 } from "aws-sdk";
 import { Registrant } from "./registrant.entity";
 
+const Bucket = process.env.AWS_S3_BUCKET;
+
 const s3 = new S3();
 
 @Injectable()
 export class FileService {
-  bucketName = `makeuc-2020-resumes`;
-
   constructor() {
-    s3.createBucket({ Bucket: this.bucketName }, (err, res) => {
-      if(err) return Logger.error(`Error creating S3 bucket: ${err}`);
+    s3.createBucket({ Bucket }, (err, res) => {
+      if(err) {
+        return Logger.error(`Error creating S3 bucket: ${err}`);
+      }
       Logger.log(`Successfully created bucket: ${res.Location}`);
     });
   }
   
   async uploadResume(file: Express.Multer.File, registrant: Registrant): Promise<string> {
     try {
-      const upload = await s3.upload({ Bucket: this.bucketName, Key: `${registrant.email}-resume.pdf`, Body: file.buffer }).promise();
+      const upload = await s3
+        .upload({ Bucket, Key: `${registrant.email}-resume.pdf`, Body: file.buffer })
+        .promise();
+
       return upload.Location;
     } catch (err) {
       Logger.error(`Error uploading file to S3: ${err.message}`);
